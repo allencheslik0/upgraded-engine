0b5ac15ab01d36585e38caf3430411bb9c97161f
Allen Cheslik <allencheslik0@gmail.com>
2021-08-30 01:13:41 -0500

    updated highscores and script

--- JS/questions.js
+++ JS/questions.js
@@ -1,32 +1,191 @@
 var questions = [
     {
-        Number: 1,
-        question: "Commonly Used Data Types DO NOT include:",
+        question: "Commonly used data types DO NOT include:",
+        options: ["Strings", "Booleans", "Alerts", "Numbers"],
         answer: "Alerts"
-        options: ["Strings", "Booleans", "Alerts", "Numbers"]    
-    }
+    },
     {
-        Number: 2,
-        question: "The condition in an if / else statement is enclosed within ______.",
-        answer: "Alerts"
-        options: [ "Quotes", "Curly Braces", "Parentheses", "Square Brackets"]
-    }
-    {
-        Number: 3,
-        question: "Arrays in JavaScript can be used to store ______.",
-        answer: "All of the above"
-        options: ["Numbers and Strings", "Other Arrays",  "Booleans", "All of the above"]       
-    }
-    {
-        Number: 4,
-        question: "String values must be enclosed within ______ when being assigned to variables.",
+        question: "The condition in an if / else statement is enclosed within ____.",
+        options: ["Quotes", "Curly brackets", "Parentheses", "Square Brackets"],
         answer: "Parentheses"
-        options: ["Quotes", "Curly Braces", "Parentheses", "Commas"]
-    }
+    },
     {
-        Number: 5,
-        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        answer: "Console.Log"
-        options: ["JavaScript", "terminal / bash", "console.log", "for loops"]
+        question: "Arrays in Javascript can be used to store ____.",
+        options: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the Above"],
+        answer: "All of the Above"
+    },
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        options: ["Commas", "Curly Brackets", "Quotes", "Parenthesis"],
+        answer: "Quotes"
+    },
+    {
+        question: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        options: ["Javascript", "Terminal / Bash", "For Loops", "Console Log"],
+        answer: "Console Log"
+    },
+
+];
+
+// Declared variables
+var score = 0;
+var questionIndex = 0;
+var Time = document.querySelector(".Time");
+var timer = document.querySelector(".startTimer");
+var questionsDiv = document.querySelector(".questions");
+var container = document.querySelector(".container");
+
+// Seconds left is 15 seconds per question:
+var secondsLeft = 75;
+// Holds interval time
+var holdInterval = 0;
+// Holds penalty time
+var penalty = 15;
+// Creates new element
+var ulCreate = document.createElement("ul");
+
+// Event Listener and zero check
+timer.addEventListener("click", function () {
+    if (holdInterval === 0) {
+        holdInterval = setInterval(function () {
+            secondsLeft--;
+            Time.textContent = "Time: " + secondsLeft;
+
+            if (secondsLeft <= 0) {
+                clearInterval(holdInterval);
+                allDone();
+                Time.textContent = "Time's up!";
+            }
+        }, 1000);
     }
-]
+    render(questionIndex);
+});
+
+// Renders QnA
+function render(questionIndex) {
+   questionsDiv.innerHTML = "";
+    ulCreate.innerHTML = "";
+    // For loops to loop through all info in array
+    for (var i = 0; i < questions.length; i++) {
+        // Appends question title only
+        var userQuestion = questions[questionIndex].question;
+        var userOptions = questions[questionIndex].options;
+        questionsDiv.textContent = userQuestion;
+    }
+    userOptions.forEach(function (newItem) {
+        var listItem = document.createElement("li");
+        listItem.textContent = newItem;
+       questionsDiv.appendChild(ulCreate);
+        ulCreate.appendChild(listItem);
+        listItem.addEventListener("click", (compare));
+    })
+}
+// Event comparison
+function compare(event) {
+    var element = event.target;
+
+    if (element.matches("li")) {
+
+        var createDiv = document.createElement("div");
+        createDiv.setAttribute("id", "createDiv");
+        if (element.textContent == questions[questionIndex].answer) {
+            score++;
+            createDiv.textContent = "Correct! The answer is:  " + questions[questionIndex].answer;
+        } else {
+            // Penalty deduction
+            secondsLeft = secondsLeft - penalty;
+            createDiv.textContent = "Wrong! The correct answer is:  " + questions[questionIndex].answer;
+        }
+
+    }
+    //What question are we on
+    questionIndex++;
+
+    if (questionIndex >= questions.length) {
+        allDone();
+        createDiv.textContent = "Fatality!" + " " + "You got  " + score + "/" + questions.length + " Correct!";
+    } else {
+        render(questionIndex);
+    }
+   questionsDiv.appendChild(createDiv);
+
+}
+function allDone() {
+   questionsDiv.innerHTML = "";
+    Time.innerHTML = "";
+
+    // Needed Heading Creation:
+    var createH1 = document.createElement("h1");
+    createH1.setAttribute("id", "createH1");
+    createH1.textContent = "All Done!"
+
+   questionsDiv.appendChild(createH1);
+
+    // paragraph creation
+    var createP = document.createElement("p");
+    createP.setAttribute("id", "createP");
+
+   questionsDiv.appendChild(createP);
+
+    // Calculates score based on time
+    if (secondsLeft >= 0) {
+        var timeRemaining = secondsLeft;
+        var createP2 = document.createElement("p");
+        clearInterval(holdInterval);
+        createP.textContent = "Your final score is: " + timeRemaining;
+
+       questionsDiv.appendChild(createP2);
+    }
+
+    // label creation
+    var createLabel = document.createElement("label");
+    createLabel.setAttribute("id", "createLabel");
+    createLabel.textContent = "Enter your initials: ";
+
+   questionsDiv.appendChild(createLabel);
+
+    // input for storage
+    var createInput = document.createElement("input");
+    createInput.setAttribute("type", "text");
+    createInput.setAttribute("id", "initials");
+    createInput.textContent = "";
+
+   questionsDiv.appendChild(createInput);
+
+    // submit
+    var createSubmit = document.createElement("button");
+    createSubmit.setAttribute("type", "submit");
+    createSubmit.setAttribute("id", "Submit");
+    createSubmit.textContent = "Submit";
+
+   questionsDiv.appendChild(createSubmit);
+
+    // Event listener to capture initials and local storage for initials and score
+    createSubmit.addEventListener("click", function () {
+        var initials = createInput.value;
+
+        if (initials === null) {
+
+            console.log("No value entered!");
+
+        } else {
+            var finalScore = {
+                initials: initials,
+                score: timeRemaining
+            }
+            console.log(finalScore);
+            var allScores = localStorage.getItem("allScores");
+            if (allScores === null) {
+                allScores = [];
+            } else {
+                allScores = JSON.parse(allScores);
+            }
+            allScores.push(finalScore);
+            var newScore = JSON.stringify(allScores);
+            localStorage.setItem("allScores", newScore);
+            // Travels to final page
+            window.location.replace("./HighScores.html");
+        }
+    });
+
+}
