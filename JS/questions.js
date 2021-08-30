4f51b2052684fc63561a7a258072dc62fdb082c1
Allen Cheslik <allencheslik0@gmail.com>
2021-08-29 19:35:56 -0500

    initial framework

---
+++ JS/questions.js
@@ -0,0 +1,32 @@
+var questions = [
+    {
+        Number: 1,
+        question: "Commonly Used Data Types DO NOT include:",
+        answer: "Alerts"
+        options: ["Strings", "Booleans", "Alerts", "Numbers"]    
+    }
+    {
+        Number: 2,
+        question: "The condition in an if / else statement is enclosed within ______.",
+        answer: "Alerts"
+        options: [ "Quotes", "Curly Braces", "Parentheses", "Square Brackets"]
+    }
+    {
+        Number: 3,
+        question: "Arrays in JavaScript can be used to store ______.",
+        answer: "All of the above"
+        options: ["Numbers and Strings", "Other Arrays",  "Booleans", "All of the above"]       
+    }
+    {
+        Number: 4,
+        question: "String values must be enclosed within ______ when being assigned to variables.",
+        answer: "Parentheses"
+        options: ["Quotes", "Curly Braces", "Parentheses", "Commas"]
+    }
+    {
+        Number: 5,
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answer: "Console.Log"
+        options: ["JavaScript", "terminal / bash", "console.log", "for loops"]
+    }
+]
